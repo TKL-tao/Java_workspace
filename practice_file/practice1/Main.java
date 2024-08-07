ac295aad73dd8212c4355f5e5f48922f28ae2b0b
unknown <zouxiaotao886@163.com>
2024-08-08 00:54:14 +0800

    update JavaLearning

--- practice_file/practice1/Main.java
+++ practice_file/practice1/Main.java
@@ -10,17 +10,22 @@ import mysecondpackage.LittleCat;
 
 public class Main extends Thread {
     public static void main(String[] args) throws InterruptedException {
-        Runnable task = () -> {System.out.println("This is thread 1");};
-        Thread thread1 = new Thread(task, "My thread 1");
-        Main thread = new Main();
-        thread.start();
-        thread1.start();
-        for (int i=0; i<10; i++) {
-            System.out.println("Outside");
-            Thread.sleep(10);
-        }
+        // Runnable task = () -> {System.out.println("This is thread 1");};  // 这是thread1的run方法
+        // Thread thread1 = new Thread(task, "My thread 1");
+        // Main thread2 = new Main();
+        // thread2.start();
+        // thread1.start();
+        // for (int i=0; i<10; i++) {
+        //     System.out.println("Outside");
+        //     Thread.sleep(10);
+        // }
+        int[][] intArr = {{1, 3, 4}, {5, 4, 1}};
+        String[] stringArr = {"a", "c", "d"};
+        System.out.println(Arrays.deepToString(intArr));
+        System.out.println(Arrays.toString(stringArr));
     }
 
+    // 这是thread2的run方法
     public void run() {
         for (int i=0; i<10; i++) {
             System.out.println("Inside");
